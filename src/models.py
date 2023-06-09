9c08b68ccf3429b388fdec76b5874bdaa8c5a27f
Juan Ramos <jars4u2@gmail.com>
2023-06-09 22:38:03 +0000

    Tables completed

--- src/models.py
+++ src/models.py
@@ -1,32 +1,48 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(25), nullable=False)
+    firstname = Column(String(25), nullable=False)
+    lastname = Column(String(25), nullable=False)
+    email = Column(String(25), nullable=False)
+# REVISAR EL UNIQUE EN email
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer, nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment_text = Column(Text, nullable=False)
+    author_id = Column(Integer, nullable=False)
+    post_id = Column(Integer, nullable=False)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    # type = Column(enumerate(iterable), nullable=False) **REVISAR EL ENUM
+    url = Column(String(50), nullable=False)
+    post_id = Column(Integer, nullable=False)
+
+
+#     def to_dict(self):
+#         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
