26fb8b86bdd02a31153bcad01a90110676485671
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-06-14 20:48:31 +0000

    relationships done

--- src/models.py
+++ src/models.py
@@ -13,32 +13,34 @@ class User(Base):
     username = Column(String(25), nullable=False)
     firstname = Column(String(25), nullable=False)
     lastname = Column(String(25), nullable=False)
-    email = Column(String(25), nullable=False)
-# REVISAR EL UNIQUE EN email
+    email = Column(String(25), unique = True, nullable=False)
+    follower= relationship('Follower',back_populates='user', uselist=True)
+    
 
 class Follower(Base):
     __tablename__ = 'follower'
     user_from_id = Column(Integer, primary_key=True)
-    user_to_id = Column(Integer, nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_text = Column(Text, nullable=False)
-    author_id = Column(Integer, nullable=False)
-    post_id = Column(Integer, nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    # type = Column(enumerate(iterable), nullable=False) **REVISAR EL ENUM
+    type = Column(nullable=False)
+    # DEFINIR EL TYPE
     url = Column(String(50), nullable=False)
-    post_id = Column(Integer, nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
 
 
 #     def to_dict(self):
