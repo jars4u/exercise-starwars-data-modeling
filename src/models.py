0c5b89f81b4169c64500b3b73edba7f87a3a48d7
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-06-26 19:00:05 +0000

    model done

--- src/models.py
+++ src/models.py
@@ -1,49 +1,46 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Text, Enum
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(25), nullable=False)
-    firstname = Column(String(25), nullable=False)
-    lastname = Column(String(25), nullable=False)
-    email = Column(String(25), unique = True, nullable=False)
-    follower= relationship('Follower',back_populates='user', uselist=True)
-    
+    email = Column(String(25), unique=True, nullable=False)
+    favorites = relationship('Favorites', back_populates='user', uselist=True)
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    user_from_id = Column(Integer, primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('user.id'))
 
-class Comment(Base):
-    __tablename__ = 'comment'
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(Text, nullable=False)
-    author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
+    name = Column(String(25), nullable = False)
+    population = Column(Integer, nullable=False)
 
-class Post(Base):
-    __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
 
-class Media(Base):
-    __tablename__ = 'media'
+class People(Base):
+    __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
-    type = Column(Enum('null'), nullable=False)
-    url = Column(String(50), nullable=False)
-    post_id = Column(Integer, ForeignKey('post.id'))
+    name = Column(String(25), nullable = False)
+    gender = Column(String(25), nullable = False)
+    birth_year = Column(String(25), nullable = False)
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=False)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=False)
 
 
 #     def to_dict(self):
 #         return {}
 
-## Draw from SQLAlchemy base
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
